7cceddfa44dc87e473d626e454ce96c1283386cd
Aaron-Imba <aiimbaquingof@gmail.com>
2026-05-25 23:53:22 -0500

    Cambios para el proyecto entidades

---
+++ Entidades/Entrada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Entidades
+{
+    public class Entrada
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Indexed] 
+        public int PeliculaId { get; set; }
+
+        public int CantidadAsientos { get; set; }
+        public string FechaFuncion { get; set; } 
+        public double TotalPagado { get; set; }
+    }
+}
---
+++ Entidades/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Entidades
+{
+    public class Pelicula
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Duracion { get; set; }
+        public string Clasificacion { get; set; }
+    }
+}
